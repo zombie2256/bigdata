9cf3d22b405cd21ed8b6a1a1ea748e0d79e47264
Sandeep Giri <sandeepgiri@gmail.com>
2017-05-30 21:31:04 +0530

    Refactored spark code so that spark part too can be tested without installing spark.

--- spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
+++ spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
@@ -1,43 +1,57 @@
 package com.cloudxlab.logparsing
 
 import org.apache.spark._
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
-import com.cloudxlab.logparsing.Utils
 
-class Utils {
-	val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
+class Utils extends Serializable {
+    val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
 
-	def containsIP(line:String):Boolean = return line matches "^([0-9\\.]+) .*$"
-	//Extract only IP
-	def extractIP(line:String):(String) = {
-	    val pattern = "^([0-9\\.]+) .*$".r
-	    val pattern(ip:String) = line
-	    return (ip.toString)
-	}
+    def containsIP(line:String):Boolean = return line matches "^([0-9\\.]+) .*$"
+    //Extract only IP
+    def extractIP(line:String):(String) = {
+        val pattern = "^([0-9\\.]+) .*$".r
+        val pattern(ip:String) = line
+        return (ip.toString)
+    }
+
+    def gettop10(accessLogs:RDD[String], sc:SparkContext):Array[(String,Int)] = {
+        //Keep only the lines which have IP
+        var ipaccesslogs = accessLogs.filter(containsIP)
+        var cleanips = ipaccesslogs.map(extractIP(_))
+        var ips_tuples = cleanips.map((_,1));
+        var frequencies = ips_tuples.reduceByKey(_ + _);
+        var sortedfrequencies = frequencies.sortBy(x => x._2, false)
+        return sortedfrequencies.take(10)
+    }
 }
+
 object EntryPoint {
-	
+	val usage = """
+    	Usage: EntryPoint file_or_directory_in_hdfs
+  	"""
+    
     def main(args: Array[String]) {
+    	
+    	if (args.length == 0) {
+    		println("Please provide the directory in hdfs as first argument.")
+    		return;
+    	}
+
         var utils = new Utils
 
         // Create a local StreamingContext with batch interval of 10 second
         val conf = new SparkConf().setAppName("WordCount")
         val sc = new SparkContext(conf);
         sc.setLogLevel("WARN")
-		var accessLogs = sc.textFile("/data/spark/project/access/access.log.45.gz")
-		accessLogs.take(10)
 
-		//Keep only the lines which have IP
-		
-		var ipaccesslogs = accessLogs.filter(utils.containsIP)
-		var cleanips = ipaccesslogs.map(utils.extractIP(_))
-		var ips_tuples = cleanips.map((_,1));
-		var frequencies = ips_tuples.reduceByKey(_ + _);
-		var sortedfrequencies = frequencies.sortBy(x => x._2, false)
-		var top10 = sortedfrequencies.take(10)
-		println("===== TOP 10 IP Addresses =====")
-		for(i <- top10){
-			println(i)
-		}
+        // var accessLogs = sc.textFile("/data/spark/project/access/access.log.45.gz")
+        var accessLogs = sc.textFile(args(0))
+        val top10 = utils.gettop10(accessLogs, sc)
+        println("===== TOP 10 IP Addresses =====")
+        for(i <- top10){
+            println(i)
+        }
     }
 }
