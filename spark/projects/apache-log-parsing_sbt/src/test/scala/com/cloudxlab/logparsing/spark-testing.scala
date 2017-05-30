6589f50ecefec45ba13d606e5f5339ff85438b92
Sandeep Giri <sandeepgiri@gmail.com>
2017-05-30 21:31:48 +0530

    Spark testing is running fine. This is on  the lines of hadoop mrunit

---
+++ spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/spark-testing.scala
@@ -0,0 +1,21 @@
+package com.cloudxlab.logparsing
+
+import org.scalatest.FunSuite
+import com.holdenkarau.spark.testing.{RDDComparisons, SharedSparkContext}
+
+class SampleTest extends FunSuite with SharedSparkContext {
+	test("Computing top10") {
+		var line1 = "121.242.40.10 - - [03/Aug/2015:06:30:52 -0400] \"POST /mod_pagespeed_beacon?url=http%3A%2F%2Fwww.knowbigdata.com%2Fpage%2Fabout-us HTTP/1.1\" 204 206 \"http://www.knowbigdata.com/page/about-us\" \"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0\""
+		var line2 = "::1 - - [11/May/2015:06:44:40 -0400] \"OPTIONS * HTTP/1.0\" 200 125 \"-\" \"Apache/2.4.7 (Ubuntu) PHP/5.5.9-1ubuntu4.7 OpenSSL/1.0.1f (internal dummy connection)\""
+
+		val utils = new Utils
+
+		val list = List(line1, line2)
+		val rdd = sc.parallelize(list);
+
+		assert(rdd.count === list.length)	
+
+		val records = utils.gettop10(rdd, sc)
+		assert(records.length === 1)			
+	}
+}
