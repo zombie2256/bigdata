da62924641834b85935d6f36617549aee8f37e11
Sandeep Giri <sandeepgiri@gmail.com>
2017-07-18 19:49:41 +0530

    Create rdd_to_df.scala

---
+++ spark/examples/dataframes/rdd_to_df.scala
@@ -0,0 +1,8 @@
+import spark.implicits._
+
+case class Person(name: String, age: Long)
+
+val txtRDD = sc.textFile("/data/spark/people.txt")
+val arrayRDD = txtRDD.map(_.split(","))
+val personRDD = arrayRDD.map(attributes => Person(attributes(0), attributes(1).trim.toInt))
+val peopleDF = personRDD.toDF()
