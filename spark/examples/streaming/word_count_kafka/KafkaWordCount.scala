380bcd5603bee7fbb396060b3bb7cf154de6e31c
Abhinav Singh <abhinav.foss@gmail.com>
2017-08-03 02:39:59 +0530

    Improved KafkaWordCount.scala code

--- spark/examples/streaming/word_count_kafka/KafkaWordCount.scala
+++ spark/examples/streaming/word_count_kafka/KafkaWordCount.scala
@@ -5,14 +5,19 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 
 object KafkaWordCount {
     def main(args: Array[String]) {
-        val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaWordCount")
-        val ssc = new StreamingContext(conf, Seconds(10))
-        val lines = KafkaUtils.createStream(ssc, "localhost:2181","spark-streaming-consumer-group", Map("test" -> 5)).map(_._2)
-        val words = lines.flatMap(_.split(" "))
-        val pairs = words.map(word => (word, 1))
-        val wordCounts = pairs.reduceByKey(_ + _)
-        wordCounts.print()
-        ssc.start()
-        ssc.awaitTermination()
+        if (args.length < 3) {
+            System.err.println("Usage: KafkaWordCount <zkQuorum> <consumerGroup> <topic>")
+                System.exit(1)
+        }
+        val Array(zkQuorum, consumerGroup, topic) = args
+            val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaWordCount")
+            val ssc = new StreamingContext(conf, Seconds(10))
+            val lines = KafkaUtils.createStream(ssc, zkQuorum, consumerGroup, Map(topic -> 1)).map(_._2)
+            val words = lines.flatMap(_.split(" "))
+            val pairs = words.map(word => (word, 1))
+            val wordCounts = pairs.reduceByKey(_ + _)
+            wordCounts.print()
+            ssc.start()
+            ssc.awaitTermination()
     }
 }
--- spark/examples/streaming/word_count_kafka/README.md
+++ spark/examples/streaming/word_count_kafka/README.md
@@ -2,5 +2,5 @@ Spark streaming using Kafka
 
 ```
 sbt package
-spark-submit --class "KafkaWordCount" --jars spark-streaming-kafka-assembly_2.10-1.5.2.jar target/scala-2.10/kafkawordcount_2.10-0.1-SNAPSHOT.jar
+spark-submit --class "KafkaWordCount" --jars spark-streaming-kafka-assembly_2.10-1.5.2.jar target/scala-2.10/kafkawordcount_2.10-0.1-SNAPSHOT.jar localhost:2181 spark-streaming-consumer-group test
 ```
