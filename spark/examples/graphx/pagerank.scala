c100f71bfd43458b319998db5e8b8ce2f8578c54
Sandeep Giri <sandeepgiri@gmail.com>
2017-08-09 16:17:15 +0530

    Update pagerank.scala

--- spark/examples/graphx/pagerank.scala
+++ spark/examples/graphx/pagerank.scala
@@ -1,9 +1,18 @@
+//export YARN_CONF_DIR=/etc/hadoop/conf/
+//export HADOOP_CONF_DIR=/etc/hadoop/conf/
+//Start Spark Shell /usr/spark2.0.2/bin/spark-shell 
+
 import org.apache.spark.graphx.GraphLoader
 
 // Load the edges as a graph
 val graph = GraphLoader.edgeListFile(sc, "/data/spark/graphx/followers.txt")
 // Run PageRank
 val ranks = graph.pageRank(0.0001).vertices
+
+//Take a look at the output
+ranks.collect()
+//res1: Array[(org.apache.spark.graphx.VertexId, Double)] = Array((4,0.15), (6,0.7), ....
+
 // Join the ranks with the usernames
 val users = sc.textFile("/data/spark/graphx/users.txt").map { line =>
   val fields = line.split(",")
