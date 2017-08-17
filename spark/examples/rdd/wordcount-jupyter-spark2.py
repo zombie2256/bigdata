fcdeb63471baa2be57933053aae038ac692a025d
Sandeep Giri <sandeepgiri@gmail.com>
2017-08-18 00:24:24 +0530

    Update wordcount-jupyter-spark2.py

--- spark/examples/rdd/wordcount-jupyter-spark2.py
+++ spark/examples/rdd/wordcount-jupyter-spark2.py
@@ -1,6 +1,4 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("PythonWordCount").getOrCreate()
-lines = spark.sparkContext.textFile("/data/mr/wordcount/input/")
+lines = sc.textFile("/data/mr/wordcount/input/")
 words = lines.flatMap(lambda x: x.split(' '))
 tuples = words.map(lambda x: (x.lower(), 1))
 result = tuples.reduceByKey(lambda x, y: x + y)
