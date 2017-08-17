84889d163054b628681862296573077affbd98db
Sandeep Giri <sandeepgiri@gmail.com>
2017-08-18 02:43:37 +0530

    Update rdd-to-df.py

--- spark/examples/python/rdd-to-df.py
+++ spark/examples/python/rdd-to-df.py
@@ -9,3 +9,12 @@ personRDD = arrayRDD.map(lambda p: Row(name=p[0], age=int(p[1])))
 peopleDF = spark.createDataFrame(personRDD)
 
 peopleDF.show()
+
+# Register the DataFrame as a temporary view
+peopleDF.createOrReplaceTempView("people")
+
+# SQL statements can be run by using the sql methods provided by Spark
+teenagersDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 19")
+
+# The columns of a row in the result can be accessed by field index
+teenagersDF.rdd.map(lambda p: "Name: " + p.name).collect()
