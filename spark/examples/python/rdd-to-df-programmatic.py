72c40d3f211da3d6c7892b562fcf6a745ad625af
Sandeep Giri <sandeepgiri@gmail.com>
2017-08-18 02:48:30 +0530

    Create rdd-to-df-programmatic.py

---
+++ spark/examples/python/rdd-to-df-programmatic.py
@@ -0,0 +1,18 @@
+# Import data types
+from pyspark.sql.types import *
+
+# Load a text file and convert each line to a Row.
+lines = sc.textFile("/data/spark/people.txt")
+parts = lines.map(lambda l: l.split(","))
+# Each line is converted to a tuple.
+people = parts.map(lambda p: (p[0], p[1].strip()))
+
+# The schema is encoded in a string.
+schemaString = "name age"
+
+fields = [StructField(field_name, StringType(), True) for field_name in schemaString.split()]
+schema = StructType(fields)
+
+# Apply the schema to the RDD.
+schemaPeople = spark.createDataFrame(people, schema)
+schemaPeople.show()
