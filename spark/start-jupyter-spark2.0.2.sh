86043e953eb48a13b4f2f780f55a67c4dea9a425
Sandeep Giri <sandeepgiri@gmail.com>
2017-08-18 05:36:37 +0530

    Update start-jupyter-spark2.0.2.sh

--- spark/start-jupyter-spark2.0.2.sh
+++ spark/start-jupyter-spark2.0.2.sh
@@ -1,6 +1,9 @@
 export SPARK_HOME="/usr/spark2.0.2/"
 export PYTHONPATH=$SPARK_HOME/python/:$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$SPARK_HOME/python/lib/pyspark.zip:$PYTHONPATH
 export PATH=/usr/local/anaconda/bin:$PATH
+export YARN_CONF_DIR=/etc/hadoop/conf/
+export HADOOP_CONF_DIR=/etc/hadoop/conf/
+
 rm nohup.out
 nohup jupyter notebook --no-browser --ip 0.0.0.0 --port 8888 &
 tail -f nohup.out &
