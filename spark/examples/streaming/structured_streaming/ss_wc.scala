f649adc7b9f82f9899f1e65ea38d019d00273366
Sandeep Giri <sandeepgiri@gmail.com>
2018-02-06 20:26:26 +0530

    Update ss_wc.scala

--- spark/examples/streaming/structured_streaming/ss_wc.scala
+++ spark/examples/streaming/structured_streaming/ss_wc.scala
@@ -20,10 +20,7 @@ val words = lines.as[String].flatMap(_.split(" "))
 val wordCounts = words.groupBy("value").count()
 
 // Start running the query that prints the running counts to the console
-val query = wordCounts.writeStream
-  .outputMode("complete")
-  .format("console")
-  .start()
+val query = wordCounts.writeStream.outputMode("complete").format("console").start()
 
 query.awaitTermination()
 
