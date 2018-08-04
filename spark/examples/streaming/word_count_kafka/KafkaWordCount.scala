7a491b39b8f7c9a1e46110b417ca8b3a7e948eae
Sandeep Giri <sandeepgiri@gmail.com>
2018-08-04 21:03:36 +0530

    Update KafkaWordCount.scala

--- spark/examples/streaming/word_count_kafka/KafkaWordCount.scala
+++ spark/examples/streaming/word_count_kafka/KafkaWordCount.scala
@@ -10,14 +10,14 @@ object KafkaWordCount {
                 System.exit(1)
         }
         val Array(zkQuorum, consumerGroup, topic) = args
-            val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaWordCount")
-            val ssc = new StreamingContext(conf, Seconds(2))
-            val lines = KafkaUtils.createStream(ssc, zkQuorum, consumerGroup, Map(topic -> 1)).map(_._2)
-            val words = lines.flatMap(_.split(" "))
-            val pairs = words.map(word => (word, 1))
-            val wordCounts = pairs.reduceByKey(_ + _)
-            wordCounts.print()
-            ssc.start()
-            ssc.awaitTermination()
+        val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaWordCount")
+        val ssc = new StreamingContext(conf, Seconds(2))
+        val lines = KafkaUtils.createStream(ssc, zkQuorum, consumerGroup, Map(topic -> 1)).map(_._2)
+        val words = lines.flatMap(_.split(" "))
+        val pairs = words.map(word => (word, 1))
+        val wordCounts = pairs.reduceByKey(_ + _)
+        wordCounts.print()
+        ssc.start()
+        ssc.awaitTermination()
     }
 }
