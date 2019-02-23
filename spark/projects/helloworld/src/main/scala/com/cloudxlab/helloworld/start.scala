f2895b5710532b09fadb1a8df53a00b56777cf71
Sandeep Giri <sandeepgiri@gmail.com>
2019-02-23 15:13:52 +0000

    Removed unnessary code

--- spark/projects/helloworld/src/main/scala/com/cloudxlab/helloworld/start.scala
+++ spark/projects/helloworld/src/main/scala/com/cloudxlab/helloworld/start.scala
@@ -4,11 +4,8 @@ object HelloWorld {
   def main(args: Array[String]) {
     val bigfile = "/data/mr/wordcount/input/big.txt" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    //val logData = spark.read.textFile(logFile).cache()
-    //val numAs = logData.filter(line => line.contains("a")).count()
-    //val numBs = logData.filter(line => line.contains("b")).count()
-    //println(s"Lines with a: $numAs, Lines with b: $numBs")
-    var rdd = spark.sparkContext.textFile(bigfile);
+    val sc = spark.sparkContext
+    var rdd = sc.textFile(bigfile)
     for(line <- rdd.take(10)){
 	print(line)
     }
