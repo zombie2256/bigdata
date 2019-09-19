3f6df9a39a828147869aab9f09e1ce835bb4a744
Sandeep Giri <sandeepgiri@gmail.com>
2019-09-19 16:42:01 +0530

    Update read-logs.py

--- spark/python/read-logs.py
+++ spark/python/read-logs.py
@@ -1,3 +1,4 @@
+# Run the following on spark console
 
 from pyspark.sql import Row
 import re
@@ -8,8 +9,12 @@ def toarry(line):
     m = re.match(regex, line)
     if m:
         g = m.groups()
-        return [Row(host=g[0], datetime=g[1], type=g[2], url=g[3], retcode=g[5], timetaken=g[6])]
-    else:
-        []
-        
-lines.flatMap(toarry).toDF().show()
+        if len(g) > 0:
+            return [Row(host=g[0], datetime=g[1], type=g[2], url=g[3], retcode=g[5], timetaken=g[6])]
+    return []
+
+
+lines = sc.textFile("/data/spark/project/access/")
+rows = lines.flatMap(toarry)
+df = rows.toDF()
+df.show()
